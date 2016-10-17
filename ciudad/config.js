2068474566f563fcbec7bddc67e3aa33711d61c1
Vale <valenvivaldi@gmail.com>
2016-10-17 20:13:52 -0300

    Agregado de archivos extraidos del truco, agregado modulo pg para postgresql

---
+++ ciudad/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db: {
+    development: 'postgresql://localhost:5432/ciudad_de_los_niños-development',
+    test: 'postgresql://localhost:5432/ciudad_de_los_niños-test'
+  }
+};
