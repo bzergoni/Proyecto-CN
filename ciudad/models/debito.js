9b2e7f681b5eb9756362158894a669d2bae97c41
jzabala <joaco_zabala@hotmail.com.ar>
2017-01-26 11:29:55 -0300

    agregado del modelo de debito

---
+++ ciudad/models/debito.js
@@ -0,0 +1,148 @@
+var pg = require('pg');
+var connectionString = "pg://postgres:postgres@localhost:5432/postgres";
+var client = new pg.Client(connectionString);
+
+function Debito(idD,nro,codBanc, nom_titular, cod_ver, t_cuenta, nombre_banco, sucursal_banco){
+  this.id = idD;
+  this.numero_cuenta=nro;
+  this.cbu=codBanc;
+  this.titular=nom_titular;
+  this.codigo=cod_ver;
+  this.cuenta=t_cuenta;
+  this.banco=nombre_banco;
+  this.sucursal=sucursal_banco;
+  this.existe=undefined;
+};
+
+
+Debito.prototype.show = function(){
+  return this.id + " | " + this.numero_cuenta+ " | " + this.cbu+ " | " + this.titular+ " | " + this.codigo+ " | " + this.cuenta+ " | " + this.banco+ " | " + this.sucursal;
+};
+
+
+Debito.prototype.cargar = function(){
+
+  var id=this.id;
+  var thisrespaldo=this;
+  client.connect(function (err) {
+    if (err){console.log(err);}
+    // execute a query on our database
+    console.log("comienza la carga de "+id);
+    console.log("SELECT * FROM ciudad_de_los_niños_development.debito where id='id'");
+  //  console.log(thisrespaldo)
+    client.query("SELECT * FROM ciudad_de_los_niños_development.debito where id='"+id+"'", function (err, result) {
+      if (err) throw err;
+      if(result.rows[0]){
+        //repetirse para todos los campos
+        ;
+        thisrespaldo.numero_cuenta=result.rows[0].nro_cuenta;
+        thisrespaldo.cbu=result.rows[0].cbu;
+        thisrespaldo.titular=result.rows[0].nombre_titular;
+        thisrespaldo.codigo=result.rows[0].codigo_verificacion;
+        thisrespaldo.cuenta=result.rows[0].tipo_cuenta;
+        thisrespaldo.banco=result.rows[0].nombre_banco;
+        thisrespaldo.sucursal=result.rows[0].sucursal_banco;
+
+      }
+      client.end(function (err) {
+        if (err) {console.log(err)};
+      });
+    });
+  });
+};
+///Esta funcion se supone que inserte o actualize la tabla
+Debito.prototype.insertar = function(){
+  var id=this.id;
+  var numero_cuenta=this.numero_cuenta;
+  var cbu=this.cbu;
+  var titular=this.titular;
+  var codigo=this.codigo;
+  var cuenta=this.cuenta;
+  var banco=this.banco;
+  var sucursal=this.sucursal;
+
+
+
+  client = new pg.Client(connectionString);
+  client.connect(function (err) {
+    if (err) {console.log(err)};
+    // execute a query on our database
+    client.query("insert into ciudad_de_los_niños_development.debito values ('"+id+"','"+numero_cuenta+"','"+cbu+"','"+titular+"','"+codigo+"','"+cuenta+"','"+banco+"','"+sucursal+"');", function (err, result) {
+      if (err){console.log(err)}
+
+
+      client.end(function (err) {
+        if (err){ console.log(err)};
+      });
+    });
+  });
+};
+
+//la funcion actualizar, subiria los cambios luego de una modificacion!
+Debito.prototype.actualizar = function(name){
+  var id=this.id;
+  var numero_cuenta=this.numero_cuenta;
+  var cbu=this.cbu;
+  var titular=this.titular;
+  var codigo=this.codigo;
+  var cuenta=this.cuenta;
+  var banco=this.banco;
+  var sucursal=this.sucursal;
+
+  client = new pg.Client(connectionString);
+  client.connect(function (err) {
+    if (err) {console.log(err)};
+    // execute a query on our database
+    client.query("update ciudad_de_los_niños_development.debito set nro_cuenta='"+numero_cuenta+"',cbu='"+cbu+"',nombre_titular='"+titular+"',codigo_verificacion='"+codigo+"',tipo_cuenta='"+cuenta+"',nombre_banco='"+banco+"',sucursal_banco='"+sucursal+"' where id='"+id+"'", function (err, result) {
+      if (err){console.log(err)}
+
+
+      client.end(function (err) {
+        if (err){ console.log(err)};
+      });
+    });
+  });
+};
+
+Debito.prototype.eliminar= function(){
+  var id=this.id;
+  client = new pg.Client(connectionString);
+  client.connect(function (err) {
+    if (err) {console.log(err)};
+    // execute a query on our database
+    client.query("delete from ciudad_de_los_niños_development.debito  where id='"+id+"'", function (err, result) {
+      if (err){console.log(err)}
+
+
+      client.end(function (err) {
+        if (err){ console.log(err)};
+      });
+
+
+    });
+  });
+};
+
+
+Debito.prototype.exist = function(){
+  var id=this.id;
+  var thisrespaldo=this;
+
+  client.connect(function (err) {
+    if (err){console.log(err);}
+    client.query("SELECT * FROM ciudad_de_los_niños_development.debito where id='"+id+"'", function (err, result) {
+      if (err) throw err;
+      if(result.rows[0]){
+        thisrespaldo.existe=true;
+      }
+      client.end(function (err) {
+        if (err) {console.log(err)};
+      });
+    });
+
+  });
+
+};
+
+
+module.exports.debito = Debito;
--- ciudad/pruebaprograma.js
+++ ciudad/pruebaprograma.js
@@ -11,6 +11,8 @@ var Persona = persona_model.persona;
 var programa_model = require("./models/programa");
 var Programa = programa_model.programa;
 
+var debito_model = require("./models/debito");
+var Debito = debito_model.debito;
 
 //
 // // connect to our database
@@ -37,20 +39,58 @@ var Programa = programa_model.programa;
 //
 
 //---------------------------------------------------------------------------------------------------------------------
+//                                          PRUEBA DE PROGRAMA
 //var programaprueba = new Programa("asado");
 //console.log(programaprueba);
 //programaprueba.cargar();
 // setTimeout(function(){console.log(programaprueba.show()); }, 15);
 
-  var programaprueba2 = new Programa("fobal socker gogogo");
+//  var programaprueba2 = new Programa("fobal socker gogogo");
 //  programaprueba2.descripcion="jugar al fobal pateando el esferico";
  //console.log(programaprueba2)
 // programaprueba2.insertar();
 //programaprueba2.cargar();
 //setTimeout(function(){programaprueba2.actualizar("fobal socker gogogo");} , 1500);
 //programaprueba2.eliminar();
-programaprueba2.exist();
-setTimeout(function(){if (programaprueba2.existe){console.log("existe el programa");} }, 1500);
+//programaprueba2.exist();
+//setTimeout(function(){if (programaprueba2.existe){console.log("existe el programa");} }, 1500);
+
+//                            -----            PRUEBA DE DEBITO         -----
+ var debitoprueba = new Debito(1);
+// console.log(debitoprueba);
+// debitoprueba.cargar();
+// setTimeout(function(){console.log(debitoprueba.show()); }, 1500);
+
+var debitoprueba2 = new Debito(2);
+// debitoprueba2.numero_cuenta=345;
+// debitoprueba2.cbu="3455786845";
+// debitoprueba2.titular="cangre burger";
+// debitoprueba2.codigo=342;
+// debitoprueba2.cuenta="debe todo";
+// debitoprueba2.banco="frances";
+// debitoprueba2.sucursal="chucuy";
+//  console.log(debitoprueba2)
+//  debitoprueba2.insertar();
+
+ // debitoprueba2.cargar();
+ // setTimeout(function(){console.log(debitoprueba2.show()); }, 1500);
+
+ // debitoprueba2.numero_cuenta=6789;
+ // debitoprueba2.cbu="12345";
+ // debitoprueba2.titular="victor corleone";
+ // debitoprueba2.codigo=678;
+ // debitoprueba2.cuenta=" todo al dia";
+ // debitoprueba2.banco="hipotecario";
+ // debitoprueba2.sucursal="ucrania";
+ // debitoprueba2.actualizar();
+ //
+ // setTimeout(function(){console.log(debitoprueba2.show()); }, 1500);
+
+//debitoprueba.eliminar();
+debitoprueba2.exist();
+setTimeout(function(){if (debitoprueba2.existe){console.log(" existe el debito");} }, 2500);
+
+
 
 //------------------------------------------------------------------------------------------------------------------
 
