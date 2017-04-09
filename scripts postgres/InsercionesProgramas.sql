568bdcbb89a9ed84e38f6106cb9f3a8ea5718464
bzergoni <bruno_z_c@hotmail.com>
2017-04-09 20:22:35 -0300

    Inserciones de programas

---
+++ scripts postgres/InsercionesProgramas.sql
@@ -0,0 +1,6 @@
+
+insert into ciudad_de_los_niños_development.programa values ('TRAPITOS AL SOL (VESTIMENTA)','Contar con prendas de vestir y calzado adecuados a distintas condiciones de uso (cotidiano, salidas, actividades específicas), contemplando las distintas épocas del año y la etapa de vida de cada niñ@.');
+
+insert into ciudad_de_los_niños_development.programa values ('SANA SANA (Salud integral)','Promover la salud integral de l@s niñ@s y adolescentes, atendiendo sus necesidades de contención, biológicas y psico-afectivas, considerando la historia personal y las condiciones orgánicas de cada un@.');
+
+insert into ciudad_de_los_niños_development.programa values ('APRENDER JUGANDO (Recreación y vida cultural)','Contribuir a la formación integral de l@sniñ@s y adolescentes, desde el juego y la expresión, la imaginación y la creatividad, promoviendo valores, descubriendo nuevas cosas y haciendo nuevas experiencias.');	
