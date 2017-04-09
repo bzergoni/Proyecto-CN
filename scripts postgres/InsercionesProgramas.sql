432dfc9ecef011ff935a7226ab5951111b8e3210
jzabala <joaco_zabala@hotmail.com.ar>
2017-04-09 20:58:24 -0300

    Inserciones de programas

--- scripts postgres/InsercionesProgramas.sql
+++ scripts postgres/InsercionesProgramas.sql
@@ -4,3 +4,15 @@ insert into ciudad_de_los_niños_development.programa values ('TRAPITOS AL SOL (
 insert into ciudad_de_los_niños_development.programa values ('SANA SANA (Salud integral)','Promover la salud integral de l@s niñ@s y adolescentes, atendiendo sus necesidades de contención, biológicas y psico-afectivas, considerando la historia personal y las condiciones orgánicas de cada un@.');
 
 insert into ciudad_de_los_niños_development.programa values ('APRENDER JUGANDO (Recreación y vida cultural)','Contribuir a la formación integral de l@sniñ@s y adolescentes, desde el juego y la expresión, la imaginación y la creatividad, promoviendo valores, descubriendo nuevas cosas y haciendo nuevas experiencias.');	
+
+
+
+insert into ciudad_de_los_niños_development.programa values ('PASITO A PASO (crianza) ','Acompañar día a día el crecimiento de los niñ@s y adolescentes atendiendo con dedicación y cariño sus necesidades esenciales (materiales y afectivas), brindándoles los cuidados necesarios y promoviendo los aprendizajes fundamentales para la construcción de su autonomía personal y la convivencia con los demás. ');
+
+
+insert into ciudad_de_los_niños_development.programa values ('NUESTRO LUGAR (Ambiente saludable) ','Mantener y mejorar continuamente los espacios de hábitat de los niñ@s que viven o asisten a la Ciudad de los Niños, creando un clima de hogar, cálido, agradable, funcional, limpio y equipado. ');
+
+
+insert into ciudad_de_los_niños_development.programa values ('DE LA SEMILLA A LA MESA (Nutrición) ','Procurar una alimentación sana, nutritiva, variada y sabrosa para tod@s los destinatari@s de la obra, ocupándose de la producción u obtención de los insumos hasta la elaboración de la comida diaria. ');
+
+
